0be8fdfcda28825d9e0d5219879434f2265b6a1d
syaidatulnazira <syaidatulnazira24@gmail.com>
2026-01-13 11:59:17 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  if (sidebar.style.display === "none") {
+    sidebar.style.display = "block";
+  } else {
+    sidebar.style.display = "none";
+  }
+}
